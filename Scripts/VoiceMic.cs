5421a5017054f1a6a2c0c7f68225de0f566747b3
MartSchilt <schilthuismart@gmail.com>
2023-11-19 12:17:51 +0100

    Implemented VoiceMic

---
+++ Scripts/VoiceMic.cs
@@ -0,0 +1,29 @@
+using Godot;
+
+namespace Kokkies;
+
+public partial class VoiceMic : AudioStreamPlayer
+{
+    private string name = "VoiceMicRecorder";
+
+    public override void _Ready()
+    {
+        var currentNumber = 0;
+        while (AudioServer.GetBusIndex(name + currentNumber.ToString()) != -1)
+        {
+            currentNumber++;
+        }
+
+        var busName = name + currentNumber.ToString();
+        var index = AudioServer.BusCount;
+
+        AudioServer.AddBus(index);
+        AudioServer.SetBusName(index, busName);
+        AudioServer.AddBusEffect(index, new AudioEffectCapture());
+        AudioServer.SetBusMute(index, true);
+
+        Bus = busName;
+        Stream = new();
+        Play();
+    }
+}
